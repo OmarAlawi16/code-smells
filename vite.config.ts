13cfb2a88f46117a2901baab3fffff891beec808
Omar Alawi <omar.alawi17@gmail.com>
2025-01-29 09:26:31 +0100

    updated vite.config and included the API:s

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    'base': '/fed24d-arbetsmetodik-inl-1-OmarAlawi16/',
+    base: '/fed24d-arbetsmetodik-inl-1-OmarAlawi16/',
+    define: {
+        'import.meta.env.VITE_APP_PROGRAMS_API': `"${process.env.VITE_APP_PROGRAMS_API}"`,
+        'import.meta.env.VITE_APP_EPISODES_API': `"${process.env.VITE_APP_EPISODES_API}"`,
+    },
 });
