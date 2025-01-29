54eca416283f6e36995dabe1026492fdbdf34923
Omar Alawi <omar.alawi17@gmail.com>
2025-01-29 10:31:12 +0100

    Removed .env from .gitinore and updated deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/fed24d-arbetsmetodik-inl-1-OmarAlawi16/',
-    define: {
-        'import.meta.env.VITE_APP_PROGRAMS_API': `"${process.env.VITE_APP_PROGRAMS_API}"`,
-        'import.meta.env.VITE_APP_EPISODES_API': `"${process.env.VITE_APP_EPISODES_API}"`,
-    },
 });
